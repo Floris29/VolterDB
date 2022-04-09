4ad847ebe01252e25a1b5249a46bdc5e2c7914ce
Floris <florismelchers@gmail.com>
2022-04-09 21:42:56 +0200

    Daar ging mn tijd

---
+++ home.php
@@ -0,0 +1,41 @@
+<?php
+
+session_start();
+if(!isset($_SESSION['username'])){
+    header('location:login.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon.png">
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<style>
+    body{
+    background: url('https://images.unsplash.com/photo-1544099858-75feeb57f01b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80');
+	  background-size: cover;
+	  background-position: center;
+	  height: 75vh;
+}
+</style>
+<body>
+    
+    <div class="container">
+
+    <a class="float-right logout" href="logout.php"> LOGOUT </a>
+
+    <h1> Welcome <?php echo $_SESSION['username']; ?> </h1>
+
+
+    </div>
+
+</body>
+</html>
--- login.php
+++ login.php
@@ -1,29 +1,52 @@
-<?php
-include_once 'header.php';
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Login And Registration </title>
+    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon.png">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <div class="login-box">
+        <div class="row">
+        <div class="col-md-6 login-left">
+            <h2> Login Here</h2>
+            <form action="validation.php" method="post">
+                <div class="form-group">
+                    <label>Username</label>
+                    <input type="text" name="user" class="form-control" required>
+                    </div>
+                <div class="form-group">
+                    <label>Password</label>
+                    <input type="password" name="password" class="form-control" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary"> Login </button>
+                </form>
+        </div> 
+    </div>
 
-  <section class="signup-form">
-      <h2>Login</h2>
-      <div class="signup-form-form">
-      <form action="includes/Login.inc.php" method="post">
-          <input type="text" name="uid" placeholder="Username/Email...">
-          <input type="password" name="pwd" placeholder="Password...">
-          <button type="submit" name="submit">Login!</button>
-</form>
-</div>
-<?php
-if (isset($_GET["error"])) {
-if ($_GET["error"] == "emptyinput") {
-  echo "<p>Fil in all fields!</p>";
-}
-else if ($_GET["error"] == "wronglogin") {
-  echo "<p>Incorrect login information</p>";
-}
-}
-?>
-</section>
+    <div class="col-md-6 login-right" >
+            <h2> Register Here</h2>
+            <form action="registration.php" method="post">
+                <div class="form-group">
+                    <label>Username</label>
+                    <input type="text" name="user" class="form-control" required>
+                    </div>
+                <div class="form-group">
+                    <label>Password</label>
+                    <input type="password" name="password" class="form-control" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary"> Register </button>
+          </form>
+    </div>
+    </div>
 
-
-<?php
-include_once 'footer.php';
-?>
+    </div>
+    
+    </div>
+</body>
+</html>
