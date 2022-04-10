94fd0999738642c2966873508bc0c0e50d6b3436
Floris <florismelchers@gmail.com>
2022-04-10 21:17:58 +0200

    Kut leven

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <div class="menu"></div>
 <ul>
     <li><b><font color="blue">Dit is zo'n goeie verificatie.</a></font></li></b>
-    <li><p><b><a href="Home.html"><font color="white">Verificatie</font> </a></ul></li></b>
+    <li><p><b><a href="login.php"><font color="white">Verificatie</font> </a></ul></li></b>
     </div>
 </body>
 <body bgcolor="black">
